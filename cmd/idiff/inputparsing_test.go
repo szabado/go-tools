4a40c198f0ad551f190306fffbf6c70edf433c22
Felix <felix.ajs@gmail.com>
2024-09-26 21:24:40 -0400

    Read line with timeout tests

--- cmd/idiff/inputparsing_test.go
+++ cmd/idiff/inputparsing_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"io"
 	"strings"
 	"testing"
 	"time"
@@ -34,4 +35,38 @@ func TestReadLineEmpty(t *testing.T) {
 	firstLine, elapsed := readLine(scanner)
 	assert.Equal("", firstLine)
 	assert.Equal(0*time.Millisecond, elapsed)
+}
+
+func TestReadLineWithTimeoutTimedOut(t *testing.T) {
+	assert := a.New(t)
+
+	reader := DelayedReader{100 * time.Millisecond, strings.NewReader("a\nb")}
+	scanner := bufio.NewScanner(reader)
+	firstLine, elapsed := readLineWithTimeout(scanner, 50*time.Millisecond)
+	assert.Equal("", firstLine)
+	assert.LessOrEqual(50*time.Millisecond, elapsed)
+	assert.Less(elapsed, 75*time.Millisecond)
+}
+
+func TestReadLineWithTimeoutNotTimedOut(t *testing.T) {
+	assert := a.New(t)
+
+	reader := DelayedReader{50 * time.Millisecond, strings.NewReader("a\nb")}
+	scanner := bufio.NewScanner(reader)
+	firstLine, elapsed := readLineWithTimeout(scanner, 100*time.Millisecond)
+	assert.Equal("a", firstLine)
+	assert.LessOrEqual(50*time.Millisecond, elapsed)
+	assert.Less(elapsed, 75*time.Millisecond)
+}
+
+type DelayedReader struct {
+	delay            time.Duration
+	underlyingReader io.Reader
+}
+
+var _ io.Reader = (*DelayedReader)(nil)
+
+func (re DelayedReader) Read(p []byte) (n int, err error) {
+	time.Sleep(re.delay)
+	return re.underlyingReader.Read(p)
 }
